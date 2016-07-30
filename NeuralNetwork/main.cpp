e187efafe91147b087b7f2356fc11f9f8a8c4087
Björn Lindstenz <blindstenz@bjorns-macbook-air.local>
2016-07-30 17:37:17 +0200

    Initial Commit

---
+++ NeuralNetwork/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NeuralNetwork
+//
+//  Created by Björn Lindstenz on 2016-07-30.
+//  Copyright © 2016 Björn Lindstenz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
